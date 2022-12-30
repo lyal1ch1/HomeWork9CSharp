d3fe988fae2aa1470422929b4c15f401bc1a88a2
lyal1ch1 <106307360+lyal1ch1@users.noreply.github.com>
2022-12-30 22:33:29 +0300

    Add files via upload

---
+++ Zadacha64.cs
@@ -0,0 +1,23 @@
+﻿﻿// // Zadacha64
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите N:");
+int number = int.Parse(Console.ReadLine()!);
+
+string NumberFromToOne(int num)
+{
+if (num == 1)
+{
+  return "1";   
+}
+else
+{
+    return num + " " + NumberFromToOne(num-1);
+}
+}
+
+Console.WriteLine(NumberFromToOne(number));
+
