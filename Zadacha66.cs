8ac7c2eec54097f9b7c44b0a712e9043784cc0fc
lyal1ch1 <106307360+lyal1ch1@users.noreply.github.com>
2022-12-30 22:34:52 +0300

    Add files via upload

---
+++ Zadacha66.cs
@@ -0,0 +1,20 @@
+﻿﻿//Zadacha 66
+
+Console.WriteLine("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+
+if (M < N)
+{
+   Console.WriteLine(SumFromFirstToSecond(M, N)); 
+} 
+else Console.WriteLine("Ошибка! Введите M < N");
+
+int SumFromFirstToSecond(int firstNum, int secondNum)
+{
+    if (firstNum == secondNum) return firstNum;
+    else return secondNum + SumFromFirstToSecond(firstNum, secondNum - 1);
+}
